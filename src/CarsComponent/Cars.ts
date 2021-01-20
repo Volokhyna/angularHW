175cb13e86d3d5008c0ab732cb99d1b9fbbc321c
Volokhyna <volokhyna28alina@gmail.com>
2021-01-20 16:57:29 +0200

    hw1

---
+++ src/CarsComponent/Cars.ts
@@ -0,0 +1,13 @@
+export let cars = [
+    { model: 'bmw', year: 2019, power: 400, color: 'red' },
+    { model: 'vw', year: 2011, power: 440, color: 'yellow' },
+    { model: 'lexus', year: 2012, power: 220, color: 'brown' },
+    { model: 'audi', year: 2013, power: 200, color: 'white' },
+    { model: 'toyota', year: 2014, power: 300, color: 'blue' },
+    { model: 'ferrari', year: 2015, power: 220, color: 'orange' },
+    { model: 'lada', year: 2016, power: 100, color: 'pink' },
+    { model: 'lanos', year: 2017, power: 140, color: 'purple' },
+    { model: 'fiat', year: 2018, power: 200, color: 'black' },
+    { model: 'volvo', year: 2010, power: 150, color: 'green' }
+];
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CarsPageComponent } from './cars-page/cars-page.component';
+import { CarComponent } from './car/car.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarsPageComponent,
+    CarComponent
   ],
   imports: [
     BrowserModule
