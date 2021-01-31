0ff8d597890e5e9976fc794c8b3e1ebfdf3a6ecc
Volokhyna <volokhyna28alina@gmail.com>
2021-01-31 20:03:37 +0200

    homework5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  name = new FormControl('', [Validators.required, Validators.minLength(4), this.wordsValidator]);
+  phone = new FormControl('', [Validators.required, Validators.minLength(13)]);
+  myForm = new FormGroup({
+    name: this.name,
+    phone: this.phone,
+    }
+  );
+
+  wordsValidator(inputData: AbstractControl): any {
+    if (inputData.value.includes('fuck')) {
+      return {error: true, msg: 'This word is not allowed'};
+    }
+    return null;
+  }
+
+  showUser(): void {
+    console.log([this.name.value, this.phone.value]);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { ComponentsComponent } from './components/components.component';
-import { UsersComponent } from './components/users/users.component';
-import { HttpClientModule} from '@angular/common/http';
-import { UserComponent } from './components/user/user.component';
-import { PostsComponent } from './components/posts/posts.component';
-import { PostComponent } from './components/post/post.component';
+import {AppComponent} from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComponentsComponent,
-    UsersComponent,
-    UserComponent,
-    PostsComponent,
-    PostComponent,
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
